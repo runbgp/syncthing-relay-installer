42f7a9c2f0a706bb4775c69a7c5ef23474aa1914
Grant <99215336+runbgp@users.noreply.github.com>
2022-02-07 15:58:34 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # syncthing-relay-installer
 A quick and simple script to deploy a Syncthing Relay Server on Ubuntu 20.04
+
+## Usage
+Download the script and make it executable.
+```
+wget https://git.axite.dev/axite/syncthing-relay-installer/raw/branch/main/syncthing-relay-installer.sh
+chmod +x syncthing-relay-installer.sh
+```
+
+
+Remember to update your node description/`-provided-by=` variable on line 28 if desired!
+
+
+Run the script.
+```
+sudo ./syncthing-relay-installer.sh
+```
+
+
+Once completed, you should see your relay show up on the relay pool which you can view here https://relays.syncthing.net/
---
+++ syncthing-relay-installer.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/syncthing/relaysrv/releases/latest)
+LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+ARTIFACT_URL="https://github.com/syncthing/relaysrv/releases/download/$LATEST_VERSION/strelaysrv-linux-amd64-$LATEST_VERSION.tar.gz"
+
+apt update && apt -y full-upgrade
+apt clean && apt autoclean && apt autoremove
+
+apt install fail2ban
+
+ufw limit in ssh
+ufw allow in 22067/tcp
+ufw allow in 22070/tcp
+ufw --force enable
+
+wget $ARTIFACT_URL
+tar zxvf strelaysrv-linux-amd64-$LATEST_VERSION.tar.gz
+mv strelaysrv-linux-amd64-$LATEST_VERSION/strelaysrv /usr/local/bin
+chmod +x /usr/local/bin/
+
+useradd relaysrv
+mkdir /etc/relaysrv
+chown relaysrv /etc/relaysrv
+
+cat << EOT >> /etc/systemd/system/relaysrv.service
+[Unit]
+Description=Syncthing Relay Daemon
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/strelaysrv -keys=/etc/relaysrv -provided-by='my little syncthing server'
+User=relaysrv
+[Install]
+WantedBy=multi-user.target
+EOT
+
+rm -rf strelaysrv-linux-amd64-$LATEST_VERSION
+rm strelaysrv-linux-amd64-$LATEST_VERSION.tar.gz
+
+systemctl daemon-reload
+systemctl enable relaysrv
+systemctl start relaysrv
